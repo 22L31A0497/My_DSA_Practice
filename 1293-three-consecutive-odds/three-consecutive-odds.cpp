bad0b83c4bdac405acc7b67c938ba85774a8703f
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-05-11 08:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (59.60%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -4,14 +4,17 @@ public:
         int count =0;
         if(arr.size()<3)return false;
         for(int i=0;i<arr.size()-2;i++){
-            for(int j=i;j<i+3;j++){
-               if(arr[j]%2!=0){
-                count++;
-            }
-            }
-            if(count == 3)return true;
-            count =0;
+        //     for(int j=i;j<i+3;j++){
+        //        if(arr[j]%2!=0){
+        //         count++;
+        //     }
+        //     }
+        //     if(count == 3)return true;
+        //     count =0;
            
+        // }
+         if(arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i+2] % 2 != 0)
+                return true;
         }
         return false;
     }
