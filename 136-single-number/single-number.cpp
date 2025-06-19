94a554b2b0d3d32a4160373504e82dd3bdd6cfe0
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-06-19 19:41:57 +0530

    Time: 27 ms (5.26%) | Memory: 24.6 MB (5.28%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,23 +1,14 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++) {  // Loop through each element
-            int nuums = nums[i];  // Store the current element
-            int count = 0;  // Initialize count to track occurrences
-
-            for(int j = 0; j < nums.size(); j++) {  // Inner loop to compare with all elements
-                if(nums[j] == nuums) {
-                    count += 1;  // Increment count when a match is found
-                }
-            }
-
-            if(count == 1) {  // If count is 1, return the number
-                return nuums;
-            }
+        map<int,int>mpp;
+        for(int num:nums){
+            mpp[num]++;
         }
-        return -1; // Default return in case no single number is found (shouldn't happen in valid inputs)
+        for(int i=0;i<nums.size();i++){
+            if(mpp[nums[i]]==1)return nums[i];
+        }
+        return 0;
+        
     }
 };
