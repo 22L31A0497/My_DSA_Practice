82409ed58566df8dde258760c9afa2f76254b1bf
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-06-23 16:06:49 +0530

    Time: 99 ms (14.54%) | Memory: 18.8 MB (37.60%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        for(int i = 0; i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(matrix[i][j]== target){
-                    return true;
-                }
-            }
+  int bins(vector<int>& nums,int tar){
+    int low=0;
+    int high=nums.size()-1;
+    while(low<=high){
+        int mid = low + (high - low) / 2;
+        if(nums[mid] == tar)return 1;
+        else if(nums[mid]>tar){
+            
+            high = mid-1;
+        }else{
+            low=mid+1;
+            
         }
-        return false;
+    }return 0;
+  }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<int>arr;
+        for(int i=0;i<m;i++){
+         if(bins(matrix[i],target)){
+            return true;
+         }
+        }
         
-    
+            return false;
+        
+       
     }
 };
