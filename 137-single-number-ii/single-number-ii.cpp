3a2d63d672f37599764302a9279607316eb9d904
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-06-25 22:47:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (23.72%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+       map<int,int>mpp;
+       for(int it:nums){
+        mpp[it]++;
+       } 
+       for(const auto it:mpp){
+        if(it.second==1)return it.first;
+       }
+       return 0;
+    }
+};
