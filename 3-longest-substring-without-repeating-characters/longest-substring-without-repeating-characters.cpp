fa8428ac0ca137216ae7095c8f7b16421b0cd3c1
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-06-28 17:47:04 +0530

    Time: 19 ms (30.67%) | Memory: 14.3 MB (22.39%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+    if(s.length()==0)return 0;
+       int maxi=INT_MIN;
+       int l=0;
+        unordered_set < char > st;
+       for(int r=0;r<s.length();r++){
+         if(st.find(s[r])!=st.end()){
+            while(l<r && st.find(s[r])!=st.end()){
+                st.erase(s[l]);
+                l++;
+            }
+         }
+         st.insert(s[r]);
+         maxi=max(maxi,r-l+1);
+       } 
+       return maxi;
+    }
+};
