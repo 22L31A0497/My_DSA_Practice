9cbba23a9f58fc10342fb0314918bac275d1f715
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-07-11 20:39:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.70%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-      int o=m-1;
-      int t=n-1;
-      int a=m+n-1;
-      while(t>=0){
-        if(o>=0 && nums1[o]>nums2[t]){
-            nums1[a]=nums1[o];
-            o--;
-            a--;
-        }else{
-            nums1[a]=nums2[t];
-            a--;
-            t--;
+        int i,j,k;
+        i=m-1;
+        j = n-1;
+        k = m+n-1;
+        if(nums1.size()==1 && nums1[0]==0){
+            nums1[0]=nums2[0];
+            
         }
-      }
+        while(i>=0 && j>=0){
+            if(nums2[j]>nums1[i]){
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+            else{
+                nums1[k] = nums1[i];
+                i--;
+                k--;
+            }
+        }
+        while(j>=0){
+          nums1[k]=nums2[j];
+          k--;
+          j--;
+        }
     }
+    //aathu coding
+    
 };
