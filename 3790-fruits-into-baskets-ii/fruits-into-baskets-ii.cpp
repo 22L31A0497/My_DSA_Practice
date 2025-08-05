7d807768a1b763b20e73ab81d76c7b4a820288c0
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-08-05 10:50:36 +0530

    Time: 2 ms (56.59%) | Memory: 48.7 MB (92.36%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        int n = fruits.size();
-        vector<bool> basketUsed(n, false); // Track which baskets are already used
-        int unplacedCount = 0;
-        for (int i = 0; i < n; i++) {
-            bool placed = false;
-            
-            for (int j = 0; j < n; j++) {
-                // Check if the basket is available and has enough capacity
-                if (!basketUsed[j] && baskets[j] >= fruits[i]) {
-                    basketUsed[j] = true; // Mark the basket as used
-                    placed = true;
-                    break;
-                }
-            }
-            
-            // If we couldn't place this fruit type
-            if (!placed) {
-                unplacedCount++;
+      int n = fruits.size();
+      int ans=n;
+      for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(fruits[i]<=baskets[j]){
+             ans--;
+                baskets[j]=0;
+                break;
             }
         }
-        
-        return unplacedCount;
+      }
+      return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+//         int n = fruits.size();
+//         vector<bool> basketUsed(n, false); // Track which baskets are already used
+//         int unplacedCount = 0;
+//         for (int i = 0; i < n; i++) {
+//             bool placed = false;
+            
+//             for (int j = 0; j < n; j++) {
+//                 // Check if the basket is available and has enough capacity
+//                 if (!basketUsed[j] && baskets[j] >= fruits[i]) {
+//                     basketUsed[j] = true; // Mark the basket as used
+//                     placed = true;
+//                     break;
+//                 }
+//             }
+            
+//             // If we couldn't place this fruit type
+//             if (!placed) {
+//                 unplacedCount++;
+//             }
+//         }
+        
+//         return unplacedCount;
+//     }
+// };
