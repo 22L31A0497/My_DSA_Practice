2dbe7c90c3f825b57d407c8d6d22865e6544dce8
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-08-05 10:28:10 +0530

    Time: 3 ms (50.65%) | Memory: 49 MB (31.74%) - LeetSync

---
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        vector<bool> basketUsed(n, false); // Track which baskets are already used
+        int unplacedCount = 0;
+        for (int i = 0; i < n; i++) {
+            bool placed = false;
+            
+            for (int j = 0; j < n; j++) {
+                // Check if the basket is available and has enough capacity
+                if (!basketUsed[j] && baskets[j] >= fruits[i]) {
+                    basketUsed[j] = true; // Mark the basket as used
+                    placed = true;
+                    break;
+                }
+            }
+            
+            // If we couldn't place this fruit type
+            if (!placed) {
+                unplacedCount++;
+            }
+        }
+        
+        return unplacedCount;
+    }
+};
