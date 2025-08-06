6595ad8996d2e8887f3a5850e0b89f5e99d4989b
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-08-06 18:30:12 +0530

    Time: 14 ms (11.83%) | Memory: 14.6 MB (43.98%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<int>st;
+        string ans ="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]!= ' '){
+                st.push(s[i]);
+            }
+            else{
+                while(!st.empty()){
+                    ans+=st.top();
+                    st.pop();
+                }
+                ans+=' ';
+            }
+          
+        }
+         
+        while(!st.empty()){
+                ans+=st.top();
+                st.pop();
+            }
+        return ans;
+
+        
+    }
+};
