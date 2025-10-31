a8abc0000d4f2256e044a2903f663ba063cec111
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-10-31 09:42:38 +0530

    Time: 3 ms (40.61%) | Memory: 26.4 MB (34.01%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,18 +1,42 @@
+//also there is a xor operation approach
+//previously submitted that approach
+
+// set based solution 
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
+        set<int>st;
         vector<int>ans;
-       map<int,int>mpp;
-       for(int num:nums){
-        mpp[num]++;
-       }
-       for(auto& it : mpp){
-        if(it.second>1){
-            ans.push_back(it.first);
+        for(int num : nums){
+            if(st.count(num)==1){
+                ans.push_back(num);
+            }
+            else{
+                st.insert(num);
+            }
         }
-       }
-       return ans;
+        return ans;
 
         
     }
 };
+
+// map based approach 
+// class Solution {
+// public:
+//     vector<int> getSneakyNumbers(vector<int>& nums) {
+//         vector<int>ans;
+//        map<int,int>mpp;
+//        for(int num:nums){
+//         mpp[num]++;
+//        }
+//        for(auto& it : mpp){
+//         if(it.second>1){
+//             ans.push_back(it.first);
+//         }
+//        }
+//        return ans;
+
+        
+//     }
+// };
